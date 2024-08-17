bbb8f7d872d2597cb58299eb3706d40c6115b9b0
Ryan McCloy <ryanmccloy@Ryans-MacBook-Air.local>
2024-08-17 11:36:47 +0100

    updated dragging logic

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,38 @@
+import Draggable from "react-draggable";
+
+import { useEffect, useRef, useState } from "react";
+
 import ModalHeading from "./ModalHeading";
 
 function Modal({ name, icon, children, onClose, onClick, isActive }) {
+  const [isDragging, setIsDragging] = useState(false);
+  const [bounds, setBounds] = useState({
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+  });
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const calculateBounds = () => {
+      if (modalRef.current) {
+        const modalRect = modalRef.current.getBoundingClientRect();
+        setBounds({
+          left: -modalRect.left,
+          top: -modalRect.top,
+          right: window.innerWidth - (modalRect.left + modalRect.width),
+          bottom: window.innerHeight - (modalRect.top + modalRect.height),
+        });
+      }
+    };
+
+    calculateBounds();
+    window.addEventListener("resize", calculateBounds);
+
+    return () => window.removeEventListener("resize", calculateBounds);
+  }, []);
+
   const position = {
     "About Me": "60%",
     Portfolio: "50%",
@@ -11,14 +43,28 @@ function Modal({ name, icon, children, onClose, onClick, isActive }) {
   const rightPosition = position[name] || "50%";
 
   return (
-    <div
-      className="absolute W95Border bg-primary-lightGray p-[3px] top-[20%] cursor-default"
-      style={{ right: rightPosition, zIndex: isActive ? 1000 : 1 }}
-      onClick={onClick}
+    <Draggable
+      handle=".modal-heading"
+      cancel=".non-draggable"
+      onStart={() => setIsDragging(true)}
+      onStop={() => setIsDragging(false)}
+      bounds={bounds}
     >
-      <ModalHeading name={name} icon={icon} onClose={onClose} />
-      {children}
-    </div>
+      <div
+        ref={modalRef}
+        className="absolute W95Border bg-primary-lightGray p-[3px] top-[20%] cursor-default"
+        style={{ right: rightPosition, zIndex: isActive ? 1000 : 1 }}
+        onClick={onClick}
+      >
+        <ModalHeading
+          name={name}
+          icon={icon}
+          onClose={onClose}
+          isDragging={isDragging}
+        />
+        {children}
+      </div>
+    </Draggable>
   );
 }
 
--- src/components/ModalHeading.jsx
+++ src/components/ModalHeading.jsx
@@ -1,14 +1,23 @@
-function ModalHeading({ name, icon, onClose }) {
+function ModalHeading({ name, icon, onClose, isDragging }) {
+  const handleButtonClick = (e) => {
+    e.stopPropagation(); // Preventing drag event from being triggered
+    onClose();
+  };
+
   return (
-    <div className="bg-primary-blue p-sm flex justify-between">
+    <div
+      className={`bg-primary-blue p-sm flex justify-between modal-heading cursor-${
+        isDragging ? "grabbing" : "grab"
+      }`}
+    >
       <div className="flex gap-[5px]">
         <img src={`icons/${icon}`} alt="icon" className="w-[25px]" />
         <p>{name}</p>
       </div>
 
       <button
-        className="W95Border bg-primary-lightGray px-sm text-primary-darkGray"
-        onClick={onClose}
+        className="W95Border bg-primary-lightGray px-sm text-primary-darkGray non-draggable"
+        onClick={handleButtonClick}
       >
         X
       </button>
