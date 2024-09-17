8b40b027eecf0d71a45233f328abf4c6a24b1c5a
Ryan McCloy <ryanmccloy@Ryans-MacBook-Air.local>
2024-09-17 19:39:52 +0100

    mobile styling

--- src/features/Toolbar/Toolbar.jsx
+++ src/features/Toolbar/Toolbar.jsx
@@ -1,17 +1,7 @@
-import { useEffect } from "react";
 import ContactMe from "./ContactMe";
 import TimeAndDate from "./TimeAndDate";
 
 function Toolbar() {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      const toolbar = document.querySelector(".toolbar");
-      toolbar.style.visibility = "visible";
-    }, 200); // Delay by 200ms
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="toolbar bg-primary-lightGray w-screen flex justify-between p-sm border-t-2 border-primary-white">
       <ContactMe />
