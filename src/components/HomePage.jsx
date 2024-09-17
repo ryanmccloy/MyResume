88d6016ec56f9314279c3cb77fb63d6696307c13
Ryan McCloy <ryanmccloy@Ryans-MacBook-Air.local>
2024-09-17 19:27:54 +0100

    trying to fix mobile style

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 function HomePage({ children }) {
+  useEffect(() => {
+    const forceRepaint = () => {
+      document.documentElement.style.display = "none";
+      document.documentElement.offsetHeight; // Trigger reflow
+      document.documentElement.style.display = "";
+    };
+
+    window.addEventListener("load", forceRepaint);
+
+    return () => window.removeEventListener("load", forceRepaint);
+  }, []);
+
   return (
     <div
       className="bg-primary-turqouise font-W95 text-main-md tracking-wide h-[calc(100dvh)] flex flex-col justify-between
